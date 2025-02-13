2238c7e4a39c4cce6d90058e724351473b50a6ec
mood350 <princiuso350@gmail.com>
2025-02-13 21:09:29 +0000

    Configuration des fichiers views.py et urls.py

--- backend/views.py
+++ backend/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import *
 
 # Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def article(request):
+    articles = Article.objects.all()
+    return render(request, 'article.html', {'articles': articles})
